7832202a431275d3cf7e729fc9a56885b15b87db
Wil Moore III <wil.moore@wilmoore.com>
2020-10-19 14:01:25 -0400

    version 0.2.1

--- tests/test_dotsplit.py
+++ tests/test_dotsplit.py
@@ -1,8 +1,7 @@
-from dotsplit import __version__
-from dotsplit import dotsplit
+from dotsplit import __version__, dotsplit
 
 def test_dotsplit_version():
-    assert __version__ == '0.2.0'
+    assert __version__ == '0.2.1'
 
 def test_dotsplit_splits():
     assert dotsplit('group.0.section.a.seat.3') == ['group', '0', 'section', 'a', 'seat', '3'], 'splits dot delimited strings'
